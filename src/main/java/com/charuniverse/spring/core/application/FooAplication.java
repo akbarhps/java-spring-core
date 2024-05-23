1c19688c2ee40dc49926e0941c913bd4af62f799
ahps <akbarputrasrg@gmail.com>
2024-05-23 10:13:59 +0700

    course zj: springboot plugin

--- src/main/java/com/charuniverse/spring/core/application/FooAplication.java
+++ src/main/java/com/charuniverse/spring/core/application/FooAplication.java
@@ -1,13 +1,8 @@
 package com.charuniverse.spring.core.application;
 
 import com.charuniverse.spring.core.data.Foo;
-import com.charuniverse.spring.core.listener.AppStartingListener;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
-
-import java.util.List;
 
 @SpringBootApplication
 public class FooAplication {
@@ -19,16 +14,16 @@ public class FooAplication {
 //        System.out.println(foo);
 //    }
 
-    public static void main(String[] args) {
-        SpringApplication springApplication = new SpringApplication(FooAplication.class);
-//        springApplication.setBannerMode(Banner.Mode.OFF);
-        springApplication.setListeners(List.of(new AppStartingListener()));
-
-        ConfigurableApplicationContext context = springApplication.run(args);
-
-        Foo foo = context.getBean(Foo.class);
-        System.out.println(foo);
-    }
+//    public static void main(String[] args) {
+//        SpringApplication springApplication = new SpringApplication(FooAplication.class);
+////        springApplication.setBannerMode(Banner.Mode.OFF);
+//        springApplication.setListeners(List.of(new AppStartingListener()));
+//
+//        ConfigurableApplicationContext context = springApplication.run(args);
+//
+//        Foo foo = context.getBean(Foo.class);
+//        System.out.println(foo);
+//    }
 
     @Bean
     public Foo foo() {
--- src/main/java/com/charuniverse/spring/core/commandapp/CommandApplication.java
+++ src/main/java/com/charuniverse/spring/core/commandapp/CommandApplication.java
@@ -1,13 +1,12 @@
 package com.charuniverse.spring.core.commandapp;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CommandApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(CommandApplication.class, args);
-    }
-
+//    public static void main(String[] args) {
+//        SpringApplication.run(CommandApplication.class, args);
+//    }
+//
 }
