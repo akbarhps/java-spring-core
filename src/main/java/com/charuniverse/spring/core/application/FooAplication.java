761ec11aa11a6ba56a0178818059d6952a53d2da
ahps <akbarputrasrg@gmail.com>
2024-05-23 09:49:13 +0700

    course zg: spring application event

--- src/main/java/com/charuniverse/spring/core/application/FooAplication.java
+++ src/main/java/com/charuniverse/spring/core/application/FooAplication.java
@@ -1,10 +1,13 @@
 package com.charuniverse.spring.core.application;
 
 import com.charuniverse.spring.core.data.Foo;
+import com.charuniverse.spring.core.listener.AppStartingListener;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
+
+import java.util.List;
 
 @SpringBootApplication
 public class FooAplication {
@@ -19,6 +22,7 @@ public class FooAplication {
     public static void main(String[] args) {
         SpringApplication springApplication = new SpringApplication(FooAplication.class);
 //        springApplication.setBannerMode(Banner.Mode.OFF);
+        springApplication.setListeners(List.of(new AppStartingListener()));
 
         ConfigurableApplicationContext context = springApplication.run(args);
 
